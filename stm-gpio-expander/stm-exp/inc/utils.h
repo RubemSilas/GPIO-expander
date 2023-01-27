9bafa164d1443558f9f8c9ff507d90c9416982ec
Silas <rsdias@hub-uea.org>
2023-01-27 08:07:18 -0400

    chore(utils): add definicoes de comunicacao
    Foram adicionadas definicoes para os handlers de I2C e UART.
    Adicionadas definicoes de tempo de timeout nas comunicoes I2C e UART.
    Tambem foram definidas alguns indicadores para operacoes de escrita e
    leitura por I2C.

--- stm-gpio-expander/stm-exp/inc/utils.h
+++ stm-gpio-expander/stm-exp/inc/utils.h
@@ -10,4 +10,14 @@
 #include "i2c.h"
 #include "usart.h"
 
+#define UART_HANDLER huart2
+#define I2C_HANDLER hi2c1
+
+#define UART_TIMEOUT (100) // milisegundos
+#define I2C_TIMEOUT (100)  // milisegundos
+
+// COMUNICATION RELATED
+#define I2C_WRITE_OPERATION 0
+#define I2C_READ_OPERATION 1
+
 #endif
