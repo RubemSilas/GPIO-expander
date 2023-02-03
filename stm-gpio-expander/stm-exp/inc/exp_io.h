7dae398b9608e8ee6cf2b0c3cfcef8d7c0e23555
Silas <rsdias@hub-uea.org>
2023-02-03 15:22:11 -0400

    chore(exp_io): add definicoes de pinos de GPIO
    Foi definida a enumeracao de mapeamento de GPIOs para cada porta no
    expansor de GPIO.

--- stm-gpio-expander/stm-exp/inc/exp_io.h
+++ stm-gpio-expander/stm-exp/inc/exp_io.h
@@ -2,5 +2,28 @@
 #define EXP_GPIOS_H
 
 #include "utils.h"
+#include "gpio.h"
+
+// PINOS
+enum exp_pins_e
+{
+    EXP_IO_PIN_0,
+    EXP_IO_PIN_1,
+    EXP_IO_PIN_2,
+    EXP_IO_PIN_3,
+    EXP_IO_PIN_4,
+    EXP_IO_PIN_5,
+    EXP_IO_PIN_6,
+    EXP_IO_PIN_7,
+    EXP_IO_PIN_8,
+    EXP_IO_PIN_9,
+    EXP_IO_PIN_10,
+    EXP_IO_PIN_11,
+    EXP_IO_PIN_12,
+    EXP_IO_PIN_13,
+    EXP_IO_PIN_14,
+    EXP_IO_PIN_15,
+};
+
 
 #endif
